3cf5c2eaca5201775ef6a53bedbc1eeacc15062f
R Narayan <nr.rnarayan@gmail.com>
2021-04-27 21:46:01 +0530

    addTeam done :sparkles:

--- commands/addTeam.js
+++ commands/addTeam.js
@@ -1,7 +1,7 @@
 import Discord from 'discord.js';
 const newPermMember = Discord.Permissions.DEFAULT;
 const newPermNonMember = Discord.Permissions.ALL;
-
+import Team from '../models/Team.js';
 async function addTeam(msg, args) {
 	//create team and send team data to mongodb
 
@@ -10,14 +10,15 @@ async function addTeam(msg, args) {
 	args.shift();
 	var teamName = args.join(' ');
 
-	console.log(
-		msg.member.roles.cache.some((role) => role.name === 'inTeam'),
-		'asdasd'
-	);
+	// Check if user is already in a team
+
 	if (msg.member.roles.cache.some((role) => role.name === 'inTeam')) {
 		msg.channel.send('You are already in a team!');
 		return;
 	}
+
+	// Createteam role
+
 	msg.channel.send('Creating Team...');
 
 	var teamRole = await guild.roles
@@ -36,6 +37,9 @@ async function addTeam(msg, args) {
 		{ id: teamRole, allow: newPermMember, type: 'role' },
 		{ id: guild.roles.everyone, deny: newPermNonMember, type: 'role' },
 	];
+
+	// Create vc and tc
+
 	await guild.channels.create(args.join('-').toLowerCase(), {
 		type: 'text',
 		topic: 'Planning',
@@ -48,11 +52,28 @@ async function addTeam(msg, args) {
 		parent: teamCategory,
 		permissionOverwrites: permissionOverwrites,
 	});
+
+	// Add Role
+
 	let role = msg.guild.roles.cache.find((r) => r.name === 'inTeam');
 	guild.member(msg.author).roles.add(role);
-
 	guild.member(msg.author).roles.add(teamRole);
-	msg.channel.send('Done!');
+	console.log(msg.author.id, msg.author.username);
+
+	// Update Database
+
+	const team = new Team({
+		id: teamRole.id,
+		name: teamRole.name,
+		owner: {
+			id: msg.author.id,
+			username: msg.author.username,
+		},
+	});
+	const createdTeam = await team.save();
+	console.log(createdTeam);
+
+	msg.channel.send('Team Created :sparkles:');
 	return Promise.resolve({ msg: 'Success!' });
 }
 
