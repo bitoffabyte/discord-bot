8a60d499ab820e2ad7ce4012a6055878eb4cca30
R Narayan <nr.rnarayan@gmail.com>
2021-04-28 17:04:28 +0530

    removed category :sparkles:

--- index.js
+++ index.js
@@ -14,38 +14,38 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
 	if (msg.author.bot) return;
-	if (msg.channel.name === 'tc')
-		if (msg.content.startsWith(prefix)) {
-			const [CMD_name, ...args] = msg.content
-				.trim()
-				.substring(prefix.length)
-				.split(/\s+/);
+	// if (msg.channel.name === 'tc')
+	if (msg.content.startsWith(prefix)) {
+		const [CMD_name, ...args] = msg.content
+			.trim()
+			.substring(prefix.length)
+			.split(/\s+/);
 
-			// if (msg.author.username !== 'bitoffabyte') {
-			// 	msg.channel.send(`Fuck you @${msg.author.username}`);
-			// 	return;
-			// }
+		// if (msg.author.username !== 'bitoffabyte') {
+		// 	msg.channel.send(`Fuck you @${msg.author.username}`);
+		// 	return;
+		// }
 
-			if (CMD_name === 'team') {
-				// create team
-				// console.log(args);
-				// console.log(msg.guild.client);
-				addTeam(msg, args);
-			}
-			if (CMD_name === 'add') {
-				joinTeam(msg, args, client);
-			}
-			if (CMD_name === 'hi') {
-				const b = msg.author.id;
-				msg.channel.send(`Hi @${msg.author} How are you?`);
-			}
-			if (CMD_name === 'rr') {
-				const b = msg.author.id;
-				msg.channel.send(
-					`https://tenor.com/view/rick-astley-rick-roll-dancing-dance-moves-gif-14097983`
-				);
-			}
+		if (CMD_name === 'team') {
+			// create team
+			// console.log(args);
+			// console.log(msg.guild.client);
+			addTeam(msg, args);
 		}
+		if (CMD_name === 'add') {
+			joinTeam(msg, args, client);
+		}
+		if (CMD_name === 'hi') {
+			const b = msg.author.id;
+			msg.channel.send(`Hi @${msg.author} How are you?`);
+		}
+		if (CMD_name === 'rr') {
+			const b = msg.author.id;
+			msg.channel.send(
+				`https://tenor.com/view/rick-astley-rick-roll-dancing-dance-moves-gif-14097983`
+			);
+		}
+	}
 });
 
 client.login(process.env.TOKEN);
